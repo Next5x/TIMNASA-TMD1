89a9cb5cf6b4c96a79c930a4ef1f8a68a7149c02
Next5x/timnasa <botuse41@gmail.com>
2025-12-07 00:49:25 +0300

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -1,1 +1,25 @@
+const { zokou } = require("../framework/zokou");
+const moment = require("moment-timezone");
+const { default: axios } = require('axios');
+//const conf = require('../set');
+
+
+zokou({ nomCom: 'ping',
+    desc: 'To check ping',
+    Categorie: 'General',
+    reaction: '🚀', 
+    fromMe: 'true', 
+
+       
+  },
+  async (dest, zk, commandeOptions) => {
+    const { ms, arg, repondre } = commandeOptions;
+    const { start} = new Date().getTime()
+    return repondre('*timnasa ₚᵢₙg ₚₒₙg ₛₚₑₑd .₉₈%*\n ```' +98+ '``` *ᴍ/s*') 
+    const { end } = new Date().getTime()
+    await zok.sendMessage('*timoth sᴘᴇᴇᴅ2 ɪs 99%!*\n ```' + (end - start) + '``` *ms*')
+  }
+)
+
+
 
